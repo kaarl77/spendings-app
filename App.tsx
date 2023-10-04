b0190b3ee3b17d2b92bae17eddf84cf2b7992a7f
Moroti Kaarl <k.moroti@rankingcoach.com>
2023-10-04 15:40:02 +0300

    push notifs

--- App.tsx
+++ App.tsx
@@ -1,28 +1,31 @@
-import 'react-native-gesture-handler';
-import {DefaultTheme, Provider as PaperProvider} from "react-native-paper";
+import "react-native-gesture-handler";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 import React from "react";
-import {NavigationContainer} from "@react-navigation/native";
-import {RootNavigator} from "./src/navigation/RootNavigator";
-import Toast from 'react-native-toast-message';
-import {Provider} from "react-redux";
-import {rootStore} from "./src/redux-stores/rootStore";
-import {persistStore} from "redux-persist";
-import {PersistGate} from "redux-persist/integration/react";
-import {LoadingState} from "./src/components/LoadingState/LoadingState";
+import { NavigationContainer } from "@react-navigation/native";
+import { RootNavigator } from "./src/navigation/RootNavigator";
+import Toast from "react-native-toast-message";
+import { Provider } from "react-redux";
+import { rootStore } from "./src/redux-stores/rootStore";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { LoadingState } from "./src/components/LoadingState/LoadingState";
+import { PushNotificationsProvider } from "./src/utils/PushNotificationsProvider";
 
 const persistor = persistStore(rootStore);
+
 export default function App() {
   return (
     <PaperProvider theme={DefaultTheme}>
       <Provider store={rootStore}>
-        <PersistGate loading={<LoadingState/>} persistor={persistor}>
+        <PersistGate loading={<LoadingState />} persistor={persistor}>
           <NavigationContainer>
-            <RootNavigator/>
-            <Toast/>
+            <PushNotificationsProvider>
+              <RootNavigator />
+              <Toast />
+            </PushNotificationsProvider>
           </NavigationContainer>
         </PersistGate>
       </Provider>
     </PaperProvider>
   );
 }
-
---
+++ src/utils/PushNotificationsProvider.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  ExpoPushToken,
+  Notification,
+  NotificationTriggerInput,
+  Subscription,
+  useLastNotificationResponse,
+} from "expo-notifications";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import { Platform } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { TabScreensNavigationProp } from "../navigation/NavigationTypes";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+type NotificationData = {
+  title: string;
+  body: string;
+  subtitle?: string;
+  delay: NotificationTriggerInput;
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function PushNotificationsProvider(props: Props) {
+  const { children } = props;
+
+  const [expoPushToken, setExpoPushToken] = useState<ExpoPushToken>();
+  const [notification, setNotification] = useState<Notification>();
+  const notificationListener = useRef<Subscription>();
+
+  const navigation =
+    useNavigation<TabScreensNavigationProp<"AddEditTransaction">>();
+
+  const lastNotificationResponse = useLastNotificationResponse();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (
+      lastNotificationResponse &&
+      lastNotificationResponse.notification.request.content.data &&
+      lastNotificationResponse.actionIdentifier ===
+        Notifications.DEFAULT_ACTION_IDENTIFIER
+    ) {
+      console.log(lastNotificationResponse.notification.request.content.data);
+      navigation.navigate("AddEditTransaction", {
+        transaction: {
+          categoryId: 1,
+          note: "test",
+          id: 1,
+          value: 1,
+          date: "2021-10-10",
+        },
+      });
+    }
+  }, [lastNotificationResponse]);
+
+  return children;
+}
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId: "3d4c2bce-9e82-4b45-8af1-a61fcbeadc3e",
+    });
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
+
+export async function scheduleCustomNotification(
+  notificationData: NotificationData
+) {
+  const { delay, body, subtitle, title } = notificationData;
+
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: title,
+      body: body,
+      subtitle: subtitle,
+    },
+    trigger: delay,
+  });
+}
